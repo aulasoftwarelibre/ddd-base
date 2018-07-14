5e2160c7f68dbb32ea3056849027edb417197d22
Sergio Gómez <sergio@uco.es>
2018-07-14 15:19:43 +0200

    Moved service classes

--- src/Service/Prooph/Plugin/EventsRecorder.php
+++ src/Service/Prooph/Plugin/EventsRecorder.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace AulaSoftwareLibre\DDD\Tests\Service\Prooph\Plugin;
+namespace AulaSoftwareLibre\DDD\Service\Prooph\Plugin;
 
 use Doctrine\Common\Collections\Collection;
 
--- src/Service/Prooph/Spec/MessageMatcher.php
+++ src/Service/Prooph/Spec/MessageMatcher.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace AulaSoftwareLibre\DDD\Tests\Service\Prooph\Spec;
+namespace AulaSoftwareLibre\DDD\Service\Prooph\Spec;
 
 use Prooph\Common\Messaging\Message;
 
