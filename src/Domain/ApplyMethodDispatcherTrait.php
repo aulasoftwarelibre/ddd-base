ecdcdd7260d31d6e52bc85023693726c161a224a
Sergio Gómez <sergio@uco.es>
2018-08-15 04:14:21 +0200

    Changed psr-4 classpath

--- src/Domain/ApplyMethodDispatcherTrait.php
+++ src/Domain/ApplyMethodDispatcherTrait.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace AulaSoftwareLibre\DDD\Domain;
+namespace AulaSoftwareLibre\DDD\BaseBundle\Domain;
 
 use Prooph\Common\Messaging\Message;
 use Prooph\EventSourcing\AggregateChanged;
