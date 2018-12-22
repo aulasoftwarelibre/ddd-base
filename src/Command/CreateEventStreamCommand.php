64558eda1aaf8b4f3280d8b59134cff678bd22dc
Sergio Gómez <sergio@uco.es>
2018-12-22 19:39:46 +0100

    Changed from prooph/bus to symfony/messenger

--- src/Command/CreateEventStreamCommand.php
+++ src/Command/CreateEventStreamCommand.php
@@ -22,7 +22,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class CreateEventStreamCommand extends Command
+final class CreateEventStreamCommand extends Command
 {
     /**
      * @var EventStore
