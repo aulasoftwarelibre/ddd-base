9057f9c0c6431a1430abe81ef369746e868e7ed2
Sergio Gómez <sergio@uco.es>
2019-02-12 06:36:37 +0100

    Added bus classes and interfaces

---
+++ src/MessageBus/EventBus.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `ddd-base` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AulaSoftwareLibre\DDD\BaseBundle\MessageBus;
+
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class EventBus
+{
+    /**
+     * @var MessageBusInterface
+     */
+    private $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
+    public function dispatch($message): Envelope
+    {
+        return $this->messageBus->dispatch($message);
+    }
+}
